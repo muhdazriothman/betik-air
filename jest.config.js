f43f25d0dab00c9d022d5b2c36f3807f86f3e401
Azri Othman <muhdazriothman@gmail.com>
2025-05-16 00:23:01 +0800

    Update dependencies and config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ module.exports = {
         '^@common/(.*)$': '<rootDir>/src/packages/common/$1',
         '^@flight/(.*)$': '<rootDir>/src/packages/flight/$1',
         '^@modules/(.*)$': '<rootDir>/src/modules/$1',
-        '^@test/(.*)$': '<rootDir>/src/test/$1'
+        '^@test/(.*)$': '<rootDir>/src/test/$1',
+        '^@user/(.*)$': '<rootDir>/src/packages/user/$1'
     },
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
